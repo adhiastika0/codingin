88703ffcf08cacc17ddedd65df649e7a904cb4ea
silveron19 <88574518+silveron19@users.noreply.github.com>
2024-06-28 09:49:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/fonts/font.js
@@ -0,0 +1,8 @@
+import { Ubuntu } from 'next/font/google';
+
+export const ubuntu = Ubuntu({
+  weight: ['300', '400', '500', '700', '300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--ubuntu-font',
+});
